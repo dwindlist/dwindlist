54f5b0077f0bdbecb5cbf194482a2137c173217c
cgvillarroel <cgvillarroel@student.apc.edu.ph>
2023-10-15 18:09:53 +0800

    chore: extract breadcrumb manager

--- dwindlist/Models/EntityManager/TodoItemManager.cs
+++ dwindlist/Models/EntityManager/TodoItemManager.cs
@@ -157,44 +157,4 @@ namespace dwindlist.Models.EntityManager
             return filteredList;
         }
     }
-
-    internal class BreadcrumbManager
-    {
-        private Dictionary<TodoItem, List<Breadcrumb>> Memo { get; set; } = new();
-
-        public List<Breadcrumb> GetBreadcrumbs(IQueryable<TodoItem> list, int id)
-        {
-            List<Breadcrumb> breadcrumbs = new();
-
-            if (id == 0)
-            {
-                return breadcrumbs;
-            }
-
-            TodoItem currentItem = list.Single(i => i.Id == id);
-
-            while (currentItem.Id != 0)
-            {
-                if (Memo.ContainsKey(currentItem))
-                {
-                    breadcrumbs.AddRange(Memo[currentItem]);
-                    break;
-                }
-
-                breadcrumbs.Add(new Breadcrumb { Id = currentItem.Id, Label = currentItem.Label });
-                Memo.Add(currentItem, breadcrumbs);
-
-                if (currentItem.ParentId == 0)
-                {
-                    break;
-                }
-
-                currentItem = list.Single(i => i.Id == currentItem.ParentId);
-            }
-
-            List<Breadcrumb> reversed = breadcrumbs.ToList();
-            reversed.Reverse();
-            return reversed;
-        }
-    }
 }
