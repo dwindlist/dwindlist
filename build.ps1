5993e4b5ff22ca08cc18c43ef6be40f6d08a1700
cgvillarroel <cgvillarroel@student.apc.edu.ph>
2023-10-28 17:50:45 +0800

    chore: build (publish) script

---
+++ build.ps1
@@ -0,0 +1,33 @@
+function script:_build {
+	$script:buildExists = (Test-Path -Path ./build)
+	if (-Not $script:buildExists) {
+		Set-Location ./dwindlist
+		dotnet publish -o ../build --self-contained
+		dotnet ef migrations bundle -o ../build/efbundle.exe --self-contained
+		Set-Location ..
+	}
+}
+
+if ($args.length -eq 0) {
+	_build
+}
+
+switch ($args[0]) {
+	"run" {
+		_build
+		Set-Location ./build
+		$script:dwindlistDbExists = (SqlLocalDB.exe i) -contains "dwindlistdb"
+		if (-Not ($script:dwindlistDbExists)) {
+			SqlLocalDB.exe c dwindlistdb
+			./efbundle.exe
+		}
+		try { ./dwindlist.exe } finally { Set-Location .. }
+	}
+	"clean" {
+		Remove-Item -r -fo ./build -ErrorAction SilentlyContinue
+		SqlLocalDB.exe p dwindlistdb
+		SqlLocalDB.exe d dwindlistdb
+		Remove-Item $HOME/dwindlist_log.ldf -ErrorAction SilentlyContinue
+		Remove-Item $HOME/dwindlist.mdf -ErrorAction SilentlyContinue
+	}
+}
