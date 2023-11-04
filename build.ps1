3b5c002ebc2342d525069db82f5183812bba0c1e
cgvillarroel <cgvillarroel@student.apc.edu.ph>
2023-11-05 03:44:28 +0800

    chore: more build script tweaks

--- build.ps1
+++ build.ps1
@@ -2,19 +2,23 @@
 $script:target = "win-x64"
 
 function script:_build {
-	$script:buildExists = (Test-Path -Path ./build)
-	if (-Not $script:buildExists) {
-		Set-Location ./dwindlist
+	$script:buildExists = (Test-Path -Path ./build/$script:target/dwindlist)
+	if ($script:buildExists) { return }
+
+	Set-Location ./dwindlist
+	try {
 		dotnet publish -r $script:target -o ../build/$script:target/dwindlist --self-contained
 		dotnet ef migrations bundle -o ../build/$script:target/dwindlist/efbundle.exe --self-contained
-		$script:dwindlistDbExists = (SqlLocalDB.exe i) -contains "dwindlistdb"
-		if (-Not ($script:dwindlistDbExists)) {
-			Set-Location ../build/$script:target/dwindlist
-			SqlLocalDB.exe c dwindlistdb
-			./efbundle.exe
-		}
-		Set-Location ../../..
-	}
+	} finally { Set-Location .. }
+
+	$script:dwindlistDbExists = (SqlLocalDB.exe i) -contains "dwindlistdb"
+	if ($script:dwindlistDbExists) { return }
+
+	Set-Location ./build/$script:target/dwindlist
+	try {
+		SqlLocalDB.exe c dwindlistdb
+		./efbundle.exe
+	} finally { Set-Location ../../.. }
 }
 
 if ($args.length -eq 0) {
@@ -31,15 +35,17 @@ switch ($args[0]) {
 		_build
 		$script:packageExists = (Test-Path -Path ./build/$script:target.zip)
 		if (-Not $script:packageExists) {
-			Set-Location ./build/$script:target
-			7z.exe a -tzip ../$script:target *
-			Set-Location ../..
+			7z.exe a -tzip ./build/$script:target ./build/$script:target/dwindlist
 		}
 	}
 	"clean" {
 		cd ./dwindlist
-		dotnet clean
-		cd ..
+		try { dotnet clean } finally { cd .. }
+		Remove-Item -r -fo ./build -ErrorAction SilentlyContinue
+	}
+	"nuke" {
+		cd ./dwindlist
+		try { dotnet clean } finally { cd .. }
 		Remove-Item -r -fo ./build -ErrorAction SilentlyContinue
 		SqlLocalDB.exe p dwindlistdb
 		SqlLocalDB.exe d dwindlistdb
